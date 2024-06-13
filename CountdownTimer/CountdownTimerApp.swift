f2869e85da725c2d6393437363a34de1e89d8580
paramagneticnaga <ishu.ishan3004@gmail.com>
2024-06-13 16:28:04 -0700

    Initial Commit

---
+++ CountdownTimer/CountdownTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownTimerApp.swift
+//  CountdownTimer
+//
+//  Created by Ishan Sharma on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct CountdownTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CountdownTimerUITests/CountdownTimerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CountdownTimerUITestsLaunchTests.swift
+//  CountdownTimerUITests
+//
+//  Created by Ishan Sharma on 6/13/24.
+//
+
+import XCTest
+
+final class CountdownTimerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
